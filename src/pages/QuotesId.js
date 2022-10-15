caa37bc85c20c52a12524fc5eb8ba640c3d45786
matanzh20000 <matanzh20000@gmail.com>
2022-10-15 18:03:54 +0300

    Second commit

--- src/pages/QuotesId.js
+++ src/pages/QuotesId.js
@@ -1,18 +1,31 @@
-import { useParams, Route } from "react-router-dom";
-import { Fragment } from "react";
-import Comments from "../components/comments/Comments";
+import { Fragment } from 'react';
+import { useParams, Route } from 'react-router-dom';
 
-const QuotesId = () => {
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import Comments from '../components/comments/Comments';
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Matan', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Zhivotovsky', text: 'Learning React is great!' },
+];
+
+const QuoteDetail = () => {
   const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
 
   return (
     <Fragment>
-      <h1>Quotes ID Page</h1>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
-        <Comments/>
+        <Comments />
       </Route>
     </Fragment>
   );
 };
 
-export default QuotesId;
+export default QuoteDetail;
